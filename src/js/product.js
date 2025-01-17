74ea8eb3c053990cda555db142e2acd390b104fe
Jessica <blackblue58@hotmail.com>
2025-01-16 17:27:35 -0800

    add middle name

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,18 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
+
+
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
+  //retrieve localstorage array. If not, set a vid array.
+  const cartArr = getLocalStorage("So-cart") || [];
+
+  //push data to array.
+  cartArr.push(product);
+
+  // Set localStorage with modified array.
+  setLocalStorage("so-cart", cartArr);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
@@ -14,3 +24,4 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+
