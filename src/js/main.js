db4518d47b27ad68df2107f03dc460dfcdc195b9
juliwhite <julietawhite30@gmail.com>
2025-02-08 20:00:47 -0700

    add function in main.js file

--- src/js/main.js
+++ src/js/main.js
@@ -5,12 +5,12 @@
 //import { updateCartCount } from "./utils.mjs";
 
 import productList from "./productList.mjs";
-import { loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter, updateCartCount } from "./utils.mjs";
 
 loadHeaderFooter();
 
 productList("#product-container", "tents-category");
 
 
-// Call the updateCartCount function when the page loads
-//document.addEventListener("DOMContentLoaded", updateCartCount);
+// Call the updateCartCount function 
+updateCartCount();
