33624dc5e554e002b8759fb9581359068377ddb4
gaydeym <gay20001@byui.com>
2025-03-11 23:47:09 -0500

    corrected styles

--- src/js/login.js
+++ src/js/login.js
@@ -8,6 +8,7 @@ const redirect = getParam("redirect") || "/";
 showWelcomeModal(); 
 
 document.querySelector("#login").addEventListener("click", () => {
+  event.preventDefault(); // This prevents the form from submitting
   const email = document.querySelector("#email").value;
   const password = document.querySelector("#password").value;
   login({ email, password }, redirect);
--- vite.config.cjs
+++ vite.config.cjs
@@ -11,14 +11,11 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
+        success: resolve(__dirname, "src/checkout/success.html"),
         product1: resolve(__dirname, "src/product_pages/index.html"),
         product_list: resolve(__dirname, "src/product-list/index.html"),
         orders: resolve(__dirname, "src/orders/index.html"),
         login: resolve(__dirname, "src/login/index.html"),
-        success: resolve(__dirname, "src/checkout/success.html"),
-
-
-
       },
     },
   },
