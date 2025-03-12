207529d4c1b8bfcf381bae4bec0cafdc96f8b1e5
Jessica <blackblue58@hotmail.com>
2025-03-11 19:06:53 -0700

    fix the mistake

--- vite.config.cjs
+++ vite.config.cjs
@@ -15,7 +15,7 @@ export default defineConfig({
         product_list: resolve(__dirname, "src/product-list/index.html"),
         orders: resolve(__dirname, "src/orders/index.html"),
         login: resolve(__dirname, "src/login/index.html"),
-        success: resolve(__dirname, "src/checkout/index.html"),
+        success: resolve(__dirname, "src/checkout/success.html"),
 
 
 
