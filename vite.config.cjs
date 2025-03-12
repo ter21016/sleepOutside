07f93e3fc3cc16a7e016b6fdab7df1e1c82e6392
Jessica <blackblue58@hotmail.com>
2025-03-11 19:01:26 -0700

    add the success dir_name and Id header/footer

--- vite.config.cjs
+++ vite.config.cjs
@@ -15,6 +15,10 @@ export default defineConfig({
         product_list: resolve(__dirname, "src/product-list/index.html"),
         orders: resolve(__dirname, "src/orders/index.html"),
         login: resolve(__dirname, "src/login/index.html"),
+        success: resolve(__dirname, "src/checkout/index.html"),
+
+
+
       },
     },
   },
